3d3bb79016e5d916f658f4edc6cd6c395c13f68a
Mujd <mujd19@gmail.com>
2018-10-27 19:36:32 -0300

    Sección 12 terminada - backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// ==================================================
+// Google
+// ==================================================
+module.exports.CLIENT_ID = '241280531289-n22485t77bqqtit4ffllagiv4vhb0gj6.apps.googleusercontent.com';
